304e539228166d2767fa71a0e00d45f9759e31eb
Konijima <14941882+Konijima@users.noreply.github.com>
2019-08-13 12:25:31 -0400

    Changed GetBalance to remove the params

--- Commands/GetBalance.d.ts
+++ Commands/GetBalance.d.ts
@@ -5,7 +5,7 @@ import { RpcResponse } from '../RpcResponse';
  */
 export interface GetBalanceRequest extends RpcRequest {
     readonly method: 'getbalance';
-    readonly params?: [string | undefined, number | undefined];
+    readonly params?: undefined;
 }
 /**
  * JSON-RPC response for the *getbalance* command.
@@ -18,12 +18,6 @@ export interface GetBalanceResponse extends RpcResponse {
  */
 export declare type GetBalanceResult = any;
 /**
- * If account is not specified, returns the server's total available balance.
- * If account is specified, returns the balance in the account.
- * Note that the account "" is not the same as leaving the parameter out.
- * The server total may be different to the balance in the default "" account.
- * @param account The selected account, or "*" for entire wallet. It may be the default account using "".
- * @param confirmations Only include transactions confirmed at least this many times.
  * @returns A JSON-RPC request for the *getbalance* command.
  */
-export declare function GetBalance(account?: string, confirmations?: number): GetBalanceRequest;
+export declare function GetBalance(): GetBalanceRequest;
--- Commands/GetBalance.js
+++ Commands/GetBalance.js
@@ -1,16 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * If account is not specified, returns the server's total available balance.
- * If account is specified, returns the balance in the account.
- * Note that the account "" is not the same as leaving the parameter out.
- * The server total may be different to the balance in the default "" account.
- * @param account The selected account, or "*" for entire wallet. It may be the default account using "".
- * @param confirmations Only include transactions confirmed at least this many times.
  * @returns A JSON-RPC request for the *getbalance* command.
  */
-function GetBalance(account, confirmations) {
-    const params = [account, confirmations].filter(v => v !== undefined);
-    return params.length === 0 ? { method: 'getbalance' } : { method: 'getbalance', params };
+function GetBalance() {
+    return { method: 'getbalance' };
 }
 exports.GetBalance = GetBalance;
