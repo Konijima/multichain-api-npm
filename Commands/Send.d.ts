bb2c06deda191e3f59e1897a321381b7e42e9b45
Konijima <14941882+Konijima@users.noreply.github.com>
2019-08-14 10:22:27 -0400

    Send back to basic

--- Commands/Send.d.ts
+++ Commands/Send.d.ts
@@ -5,7 +5,7 @@ import { RpcResponse } from '../RpcResponse';
  */
 export interface SendRequest extends RpcRequest {
     readonly method: 'send';
-    readonly params: [string, number];
+    readonly params?: any[];
 }
 /**
  * JSON-RPC response for the *send* command.
@@ -18,10 +18,7 @@ export interface SendResponse extends RpcResponse {
  */
 export declare type SendResult = any;
 /**
- * Send an amount (or several asset amounts) to a given address. The amount is
- * a real and is rounded to the nearest 0.00000001
- * @param to The address to send to.
- * @param nativeAmount The amount in native currency to send. eg 0.1
+ * Send an amount (or several asset amounts) to a given address.
  * @returns A JSON-RPC request for the *send* command.
  */
-export declare function Send(to: string, nativeAmount: number): SendRequest;
+export declare function Send(...params: any[]): SendRequest;
--- Commands/Send.js
+++ Commands/Send.js
@@ -1,13 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * Send an amount (or several asset amounts) to a given address. The amount is
- * a real and is rounded to the nearest 0.00000001
- * @param to The address to send to.
- * @param nativeAmount The amount in native currency to send. eg 0.1
+ * Send an amount (or several asset amounts) to a given address.
  * @returns A JSON-RPC request for the *send* command.
  */
-function Send(to, nativeAmount) {
-    return { method: 'send', params: [to, nativeAmount] };
+function Send(...params) {
+    return params.length === 0 ? { method: 'send' } : { method: 'send', params };
 }
 exports.Send = Send;
